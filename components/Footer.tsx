c0124d5f9793c78ca4312133254a1c038121f6e6
Praise Abu <67503488+abupraise@users.noreply.github.com>
2024-05-05 21:14:00 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => (
       <div className='flex flex-col justify-start items-start gap-6'>
         <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain' />
         <p className='text-base text-gray-700'>
-          Carhub 2023 <br />
+          Carhub 2024 <br />
           All Rights Reserved &copy;
         </p>
       </div>
@@ -35,7 +35,7 @@ const Footer = () => (
     </div>
 
     <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-      <p>@2023 CarHub. All rights reserved</p>
+      <p>@2024 CarHub. All rights reserved</p>
 
       <div className="footer__copyrights-link">
         <Link href="/" className="text-gray-500">
